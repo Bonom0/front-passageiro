112cce04da392e3c88b058195d0ffb074626ed77
caio <caioazbonomo@gmail.com>
2025-05-20 20:18:25 -0300

    Importado core, passageiro service e model, common e router. Criado construtor, ngOnInit e função carregarPassageiro.

--- src/app/transporte/listagem/listagem.component.ts
+++ src/app/transporte/listagem/listagem.component.ts
@@ -1,11 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PassageiroService } from '../passageiro.service';
+import { Passageiro } from '../passageiro.model';
+import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-listagem',
-  imports: [],
+  imports: [CommonModule, RouterLink],
   templateUrl: './listagem.component.html',
-  styleUrl: './listagem.component.css'
+  styleUrl: './listagem.component.css',
 })
-export class ListagemComponent {
+export class ListagemComponent implements OnInit {
+  passageiros: Passageiro[] = [];
 
+  constructor(private passageiroService: PassageiroService) {}
+
+  ngOnInit(): void {
+    this.carregarPassageiro();
+  }
+
+  carregarPassageiro(): void {
+    this.passageiroService.listarPassageiro().subscribe((res) => {
+      //subscribe > espera um retorno para prosseguir
+      this.passageiros = res;
+    });
+  }
 }
