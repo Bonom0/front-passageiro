6e2f3c16eccf366d7e23b3cb6141ac377c9c44bb
caio <caioazbonomo@gmail.com>
2025-05-20 20:12:23 -0300

    Importado componente de Listagem, Cadastro e Edição. Adicionado routes e path para cada componente

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { ListagemComponent } from './transporte/listagem/listagem.component';
+import { CadastroComponent } from './transporte/cadastro/cadastro.component';
+import { EdicaoComponent } from './transporte/edicao/edicao.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'listagem', pathMatch: 'full' },
+
+  { path: 'listagem', component: ListagemComponent },
+
+  { path: 'cadastro', component: CadastroComponent },
+
+  { path: 'edicao/:id', component: EdicaoComponent },
+];
