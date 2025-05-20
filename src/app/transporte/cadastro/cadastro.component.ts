82cbbc651b2a04bc2978cef81305cc8e147a5271
caio <caioazbonomo@gmail.com>
2025-05-20 20:14:50 -0300

    Importado CommonModule, model Passageiro, FormsModule, service e Router. Criado construtor e função de salvar.

--- src/app/transporte/cadastro/cadastro.component.ts
+++ src/app/transporte/cadastro/cadastro.component.ts
@@ -1,11 +1,43 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Passageiro } from '../passageiro.model';
+import { FormsModule } from '@angular/forms';
+import { PassageiroService } from '../passageiro.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-cadastro',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './cadastro.component.html',
-  styleUrl: './cadastro.component.css'
+  styleUrl: './cadastro.component.css',
 })
 export class CadastroComponent {
+  passageiro: Passageiro = {
+    id: '',
+    nome: '',
+    cpf: '',
+    senha: '',
+    cep: '',
+    rua: '',
+    contato: '',
+    horario_embarque: new Date(),
+    id_motorista: '',
+    ativo: true,
+    dta_insert: new Date(),
+    tipo: '',
+    email: '',
+  };
 
+  constructor(
+    private passageiroService: PassageiroService,
+    private router: Router
+  ) {}
+
+  salvar() {
+    this.passageiroService
+      .cadastrarPassageiro(this.passageiro)
+      .subscribe(() => {
+        this.router.navigate(['/listagem']);
+      });
+  }
 }
