6c86492bce13e51536c995882d75c39c6bab6565
caio <caioazbonomo@gmail.com>
2025-06-04 20:21:32 -0300

    Corrigido component

--- src/app/layout/sidebar/sidebar.component.ts
+++ src/app/layout/sidebar/sidebar.component.ts
@@ -1,19 +1,8 @@
 import { Component } from '@angular/core';
-import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-sidebar',
-  imports: [RouterLink],
   templateUrl: './sidebar.component.html',
-  styleUrl: './sidebar.component.css',
-  template: `
-    <nav class="side">
-      <ul>
-        <li>
-          <a routerLink="/empresa/cadastro">Empresa</a>
-        </li>
-      </ul>
-    </nav>
-  `,
+  styleUrls: ['./sidebar.component.css'],
 })
 export class SidebarComponent {}
