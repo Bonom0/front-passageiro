54c1608f19e39c83a29554b2063d6acfced601ab
caio <caioazbonomo@gmail.com>
2025-06-04 20:07:08 -0300

    Tentativa de importar sidebar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,32 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SidebarComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  // template: `
+  //   <div class="wrapper">
+  //     <app-sidebar />
+  //     <main class="content">
+  //       <router-outlet />
+  //     </main>
+  //   </div>
+  // `,
+  // styles: [
+  //   `
+  //     .wrapper {
+  //       display: flex;
+  //     }
+
+  //     .content {
+  //       flex: 1;
+  //       padding: 16px;
+  //     }
+  //   `,
+  // ],
 })
 export class AppComponent {
   title = 'projeto_transporte';
