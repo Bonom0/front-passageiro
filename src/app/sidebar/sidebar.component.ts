4de1410c02599fb47483e5907919491eaf9176bb
Bonom0 <caioazbonomo@gmail.com>
2025-06-09 20:13:56 -0300

    Arquivos para sidebar

---
+++ src/app/sidebar/sidebar.component.ts
@@ -0,0 +1,29 @@
+import { Component, EventEmitter, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
+
+@Component({
+  selector: 'app-sidebar',
+  standalone: true,
+  imports: [CommonModule, RouterModule],
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.css']
+})
+export class SidebarComponent {
+  isCollapsed = false;
+  expandedGroups = {
+    cadastro: true,
+    listagem: true,
+  };
+
+  @Output() collapsedChange = new EventEmitter<boolean>();
+
+  toggleSidebar() {
+    this.isCollapsed = !this.isCollapsed;
+    this.collapsedChange.emit(this.isCollapsed);
+  }
+
+  toggleGroup(group: 'cadastro' | 'listagem') {
+    this.expandedGroups[group] = !this.expandedGroups[group];
+  }
+}
