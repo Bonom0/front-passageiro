f5ba52f8e70589655efe5cbc4cb200c8192dcdcd
Bonom0 <caioazbonomo@gmail.com>
2025-06-09 20:17:02 -0300

    Adicionado importações para verificar qual tela irá ser exibida

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,33 +1,28 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { CommonModule } from '@angular/common';     // para *ngIf
+import { RouterModule } from '@angular/router';     // para <router-outlet>
+import { LoginComponent } from './login/login.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, SidebarComponent],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css',
-  // template: `
-  //   <div class="wrapper">
-  //     <app-sidebar />
-  //     <main class="content">
-  //       <router-outlet />
-  //     </main>
-  //   </div>
-  // `,
-  // styles: [
-  //   `
-  //     .wrapper {
-  //       display: flex;
-  //     }
-
-  //     .content {
-  //       flex: 1;
-  //       padding: 16px;
-  //     }
-  //   `,
-  // ],
+  standalone: true,
+  imports: [
+    CommonModule,
+    RouterModule,
+    LoginComponent,
+    SidebarComponent
+  ],
+  template: `
+    <app-login *ngIf="!isLoggedIn" (loginSuccess)="handleLogin()"></app-login>
+    <app-sidebar *ngIf="isLoggedIn"></app-sidebar>
+    <router-outlet *ngIf="isLoggedIn"></router-outlet>
+  `
 })
 export class AppComponent {
-  title = 'projeto_transporte';
+  isLoggedIn = false;
+
+  handleLogin() {
+    this.isLoggedIn = true;
+  }
 }
