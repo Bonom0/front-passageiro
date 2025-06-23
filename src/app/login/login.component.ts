955673c3154c6174dd5ce3ea75a57365f2d7394a
Bonom0 <caioazbonomo@gmail.com>
2025-06-23 11:58:01 -0300

    Ajustado para realizar chamada no back-end

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,26 +1,52 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [CommonModule, FormsModule],
+  imports: [CommonModule, FormsModule, HttpClientModule],
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
   email = '';
   password = '';
-  userType = 'Operador';
+  userType = '';
+  errorMessage = '';
 
-  @Output() loginSuccess = new EventEmitter<{ userType: string; email: string; password: string }>();
+  @Output() loginSuccess = new EventEmitter<{ userType: string; email: string; token: string }>();
+
+  constructor(private http: HttpClient) {}
 
   onSubmit() {
-    if (this.email && this.password.length >= 6) {
-      this.loginSuccess.emit({ userType: this.userType, email: this.email, password: this.password });
-    } else {
-      alert('Email ou senha inválidos');
-    }
+  if (this.email && this.password.length >= 6) {
+    const loginData = {
+      email: this.email,
+      password: this.password,
+      userType: this.userType
+    };
+
+    this.http.post<any>('http://localhost:3000/auth/login', loginData).subscribe({
+      next: (response) => {
+        if (response?.access_token) {
+          localStorage.setItem('token', response.access_token);
+          this.loginSuccess.emit({ userType: this.userType, email: this.email, token: response.access_token });
+        } else {
+          this.errorMessage = 'Resposta inválida do servidor.';
+        }
+      },
+      error: (err) => {
+        if (err.status === 401) {
+          this.errorMessage = 'Email ou senha incorretos.';
+        } else {
+          this.errorMessage = 'Erro ao tentar logar. Tente novamente.';
+        }
+      }
+    });
+  } else {
+    this.errorMessage = 'Email ou senha inválidos.';
   }
+}
 }
