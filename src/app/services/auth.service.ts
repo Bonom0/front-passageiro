9ac8bc833e33cbf820edf43e52a2eda4cc793b2a
Bonom0 <caioazbonomo@gmail.com>
2025-06-23 11:55:45 -0300

    Arquivo responsável por armazenar o token de login

---
+++ src/app/services/auth.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { tap } from 'rxjs/operators';
+import { Observable, BehaviorSubject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private tokenKey = 'access_token';
+  private loggedIn = new BehaviorSubject<boolean>(this.hasToken());
+
+  constructor(private http: HttpClient) {}
+
+  login(email: string, password: string, userType: string): Observable<any> {
+    return this.http.post<{ access_token: string }>('/auth/login', { email, password, userType })
+      .pipe(
+        tap(response => {
+          localStorage.setItem(this.tokenKey, response.access_token);
+          this.loggedIn.next(true);
+        })
+      );
+  }
+
+  logout() {
+    localStorage.removeItem(this.tokenKey);
+    this.loggedIn.next(false);
+  }
+
+  getToken(): string | null {
+    return localStorage.getItem(this.tokenKey);
+  }
+
+  isLoggedIn(): Observable<boolean> {
+    return this.loggedIn.asObservable();
+  }
+
+  private hasToken(): boolean {
+    return !!localStorage.getItem(this.tokenKey);
+  }
+}
