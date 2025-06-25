d676c38945fdafe27f1e449f5d884b432ddc1ead
Bonom0 <caioazbonomo@gmail.com>
2025-06-25 15:22:30 -0300

    Alterado fução onSubmit, que antes estava fazendo requisição http direto pelo Angular, agora faz pela api e está usando o AuthService

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,7 +1,8 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-login',
@@ -18,35 +19,24 @@ export class LoginComponent {
 
   @Output() loginSuccess = new EventEmitter<{ userType: string; email: string; token: string }>();
 
-  constructor(private http: HttpClient) {}
+  constructor(private authService: AuthService) {}
 
   onSubmit() {
-  if (this.email && this.password.length >= 6) {
-    const loginData = {
-      email: this.email,
-      password: this.password,
-      userType: this.userType
-    };
-
-    this.http.post<any>('http://localhost:3000/auth/login', loginData).subscribe({
-      next: (response) => {
-        if (response?.access_token) {
-          localStorage.setItem('token', response.access_token);
+    if (this.email && this.password.length >= 6) {
+      this.authService.login(this.email, this.password, this.userType).subscribe({
+        next: (response) => {
           this.loginSuccess.emit({ userType: this.userType, email: this.email, token: response.access_token });
-        } else {
-          this.errorMessage = 'Resposta inválida do servidor.';
+        },
+        error: (err) => {
+          if (err.status === 401) {
+            this.errorMessage = 'Email ou senha incorretos.';
+          } else {
+            this.errorMessage = 'Erro ao tentar logar. Tente novamente.';
+          }
         }
-      },
-      error: (err) => {
-        if (err.status === 401) {
-          this.errorMessage = 'Email ou senha incorretos.';
-        } else {
-          this.errorMessage = 'Erro ao tentar logar. Tente novamente.';
-        }
-      }
-    });
-  } else {
-    this.errorMessage = 'Email ou senha inválidos.';
+      });
+    } else {
+      this.errorMessage = 'Email ou senha inválidos.';
+    }
   }
-}
 }
