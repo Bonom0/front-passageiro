c586d9c74d3d329f12a8e4e3ef5176e7592de45c
Bonom0 <caioazbonomo@gmail.com>
2025-06-25 15:19:47 -0300

    Alterado variável isLoggedIn de boolean para Observable de acordo com o AuthService

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,10 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';     // para *ngIf
-import { RouterModule } from '@angular/router';     // para <router-outlet>
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule, Router } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
+import { AuthService } from './services/auth.service';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -13,16 +15,24 @@ import { SidebarComponent } from './sidebar/sidebar.component';
     LoginComponent,
     SidebarComponent
   ],
-  template: `
-    <app-login *ngIf="!isLoggedIn" (loginSuccess)="handleLogin()"></app-login>
-    <app-sidebar *ngIf="isLoggedIn"></app-sidebar>
-    <router-outlet *ngIf="isLoggedIn"></router-outlet>
-  `
+  templateUrl: './app.component.html',  // Usa o HTML externo, como deve ser
+  styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  isLoggedIn = false;
+export class AppComponent implements OnInit {
+  isLoggedIn$!: Observable<boolean>;
+  sidebarCollapsed = false;
+  
+  constructor(private router: Router, private authService: AuthService) {}
 
-  handleLogin() {
-    this.isLoggedIn = true;
+  ngOnInit() {
+    this.isLoggedIn$ = this.authService.isLoggedIn();
+  }
+
+  onLogin() {
+    this.router.navigate(['/dashboard']);
+  }
+
+  onSidebarToggle(collapsed: boolean) {
+    this.sidebarCollapsed = collapsed;
   }
 }
