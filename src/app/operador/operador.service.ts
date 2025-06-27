f009b215377a183f95b354fd11cfbe8aae45ab15
Bonom0 <caioazbonomo@gmail.com>
2025-06-27 17:18:35 -0300

    Corrigido exibição de Empresa e Tipo usuário que não estavam exibindo

--- src/app/operador/operador.service.ts
+++ src/app/operador/operador.service.ts
@@ -2,6 +2,8 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Operador } from './operador.model';
+import { EmpresaService } from '../empresa/empresa.service';
+import { TipoUsuarioService } from '../tipousuario/tipousuario.service';
 
 @Injectable({
   providedIn: 'root',
@@ -9,7 +11,11 @@ import { Operador } from './operador.model';
 export class OperadorService {
   private apiURL = 'http://localhost:3000/operador';
 
-  constructor(private http: HttpClient) {}
+  constructor(
+    private http: HttpClient,
+    private empresaService: EmpresaService,
+    private tipoUsuarioService: TipoUsuarioService
+  ) {}
 
   listarOperador(): Observable<Operador[]> {
     return this.http.get<Operador[]>(this.apiURL);
@@ -32,5 +38,17 @@ export class OperadorService {
 
   deletarOperador(id: string): Observable<void> {
     return this.http.delete<void>(`${this.apiURL}/${id}`);
+  }
+
+  carregarEmpresa(callback?: () => void): void {
+    this.empresaService.listarEmpresa().subscribe((empresa) => {
+      if (callback) callback();
+    });
+  }
+
+  carregarTiposUsuario(callback?: () => void): void {
+    this.tipoUsuarioService.listarTipoUsuario().subscribe((tipos) => {
+      if (callback) callback();
+    });
   }
 }
