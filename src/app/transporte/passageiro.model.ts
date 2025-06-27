b93cb55989fda1808bd60f81dcef031dc0bab976
Bonom0 <caioazbonomo@gmail.com>
2025-06-27 17:25:28 -0300

    Alterado id para opcional, campos de data agora são string

--- src/app/transporte/passageiro.model.ts
+++ src/app/transporte/passageiro.model.ts
@@ -1,15 +1,15 @@
 export interface Passageiro {
-  id: string;
+  id?: string;
   nome: string;
   cpf: string;
   senha: string;
   cep: string;
   rua: string;
   contato: string;
-  horario_embarque: Date;
+  horario_embarque: string;
   id_motorista: string;
   ativo: boolean;
-  dta_insert: Date;
-  tipo: string;
+  dta_insert: string;
+  tipo: string | { id: string; descricao: string };
   email: string;
 }
