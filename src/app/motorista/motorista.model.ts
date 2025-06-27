fbc38db33281211d67b35f8073eb0e998d16ad77
Bonom0 <caioazbonomo@gmail.com>
2025-06-27 17:13:16 -0300

    Alterado id para opcional e adicionado campo tipo any opcional

--- src/app/motorista/motorista.model.ts
+++ src/app/motorista/motorista.model.ts
@@ -1,9 +1,10 @@
 export interface Motorista {
-  id: string;
+  id?: string;
   nome: string;
   cpf: string;
   contato: string;
   email: string;
   senha: string;
   tipo_usuario_id: string;
+  tipo?: any;
 }
